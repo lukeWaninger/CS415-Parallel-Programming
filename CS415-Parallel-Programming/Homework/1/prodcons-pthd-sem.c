0455f3ec91be940cb6b2d29bdc4f8f22093ca450
lukeWaninger <luke.waninger@gmail.com>
2017-01-29 23:11:35 -0800

    .

--- CS415-Parallel-Programming/Homework/1/prodcons-pthd-sem.c
+++ CS415-Parallel-Programming/Homework/1/prodcons-pthd-sem.c
@@ -12,6 +12,8 @@
 #include <pthread.h>
 #include <sched.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <errno.h>
 #include "task-queue.h"
 
 // function declarations
@@ -19,21 +21,13 @@ void consumer(void*);
 void producer(void);
 
 // default values
-int numConsumers = 1;    // default number of consumers
-int queue_cap    = 20;   // maximum number of items in the queue
-int num_tasks    = 100;  // number of tasks to complete
-queue_t *queue;          // the queue
-pthread_mutex_t buffer;  // lock for task load/unload
-
-struct track {
-  pthread_mutex_t lock;
-  int num;
-};
-struct track *free_count;
-struct track *task_count;
-
-void post(struct track*);
-void dec(struct track*);
+int numConsumers = 1;   // default number of consumers
+int queue_cap    = 20;  // maximum number of items in the queue
+int num_tasks    = 100; // number of tasks to complete
+queue_t *queue;         // the queue
+sem_t sem_task_count;   // semaphore to count waiting tasks
+sem_t sem_free_count;   // semaphore to count free tasks in buffer
+pthread_mutex_t mutex;  // lock for task load/unload
 
 int main(int argc, char **argv) {
   if (argc > 1) {
@@ -45,16 +39,14 @@ int main(int argc, char **argv) {
   // initialize the producer consumer queue
   queue = init_queue(queue_cap);
 
-  // initialize the locks
-  pthread_mutex_init(&buffer, NULL);
-
-  struct track *free_count = malloc(sizeof(struct track));
-  pthread_mutex_init(&free_count->lock, NULL);
-  free_count->num = 20;
-
-  struct track *task_count = malloc(sizeof(struct track));
-  pthread_mutex_init(&task_count->lock, NULL);
-  task_count->num = 0;
+  // initialize the semaphores
+  // exit with an error code if the semaphore fails to init
+  if (sem_init(&sem_task_count, 0, 0) != 0 ||
+      sem_init(&sem_free_count, 0, queue_cap) != 0)
+    exit(EAGAIN);
+  
+  // initialize the lock
+  pthread_mutex_init(&mutex, NULL);
 
   // create consumer threads
   pthread_t *threads = 
@@ -70,8 +62,6 @@ int main(int argc, char **argv) {
   for (long k = 0; k < numConsumers; k++)
     pthread_join(threads[k], NULL);
   
-  free(free_count);
-  free(task_count);
   exit(0);
 }
 
@@ -81,26 +71,22 @@ void producer(void) {
   int t_num = 1;
   while(t_num <= num_tasks) {
     task_t *t = create_task(t_num, t_num);   // create task
-    
-    while (free_count->num == 0);
-    dec(free_count);
-    pthread_mutex_lock(&buffer);
+    sem_wait(&sem_free_count);               // wait and decrease sem
+    pthread_mutex_lock(&mutex);              // lock the queue
     add_task(queue, t);                      // add the task
-    pthread_mutex_unlock(&buffer);
-    post(task_count);    
+    pthread_mutex_unlock(&mutex);            // unlock the queue
+    sem_post(&sem_task_count);               // increase sem count
     ++t_num;
   }
   
   // lock, send termination tasks, unlock
   for (int i = 0; i < numConsumers; i++) {
     task_t *terminate = create_task(-1, -1); // create task
-
-    while (free_count->num == 0);
-    dec(free_count);
-    pthread_mutex_lock(&buffer);             // lock the queue
+    sem_wait(&sem_free_count);               // wait for an opening
+    pthread_mutex_lock(&mutex);              // lock the queue
     add_task(queue, terminate);              // add to queue
-    pthread_mutex_unlock(&buffer);           // unlock the queue
-    post(task_count);                        // increase count
+    pthread_mutex_unlock(&mutex);            // unlock the queue
+    sem_post(&sem_task_count);               // increase sem count
   }
 }
 
@@ -110,13 +96,12 @@ void consumer(void *p) {
 
   int t_consumed = 0;
   while (1) {
-    while (task_count->num == 0);
-    dec(task_count);
-    pthread_mutex_lock(&buffer);              // lock the queue
-    task_t *task = remove_task(queue);        // take a task
-    pthread_mutex_unlock(&buffer);            // unlock
-    post(free_count);
-
+    sem_wait(&sem_task_count);               // wait for a task
+    pthread_mutex_lock(&mutex);              // lock the queue
+    task_t *task = remove_task(queue);       // take a task
+    pthread_mutex_unlock(&mutex);            // unlock
+    sem_post(&sem_free_count);               // increase the free sem
+ 
     // check for termination task
     if (task->low == -1 && task->high == -1) {
       printf("C[%ld]: %d\n", k, t_consumed);
@@ -126,16 +111,3 @@ void consumer(void *p) {
     ++t_consumed;
   }
 }
-
-void post(struct track *p) {
-   pthread_mutex_lock(&p->lock);
-   p->num++;
-   pthread_mutex_unlock(&p->lock);
-}
-
-void dec(struct track *p) {
-  pthread_mutex_lock(&p->lock);
-  p->num--;
-  pthread_mutex_unlock(&p->lock);
-}
-
