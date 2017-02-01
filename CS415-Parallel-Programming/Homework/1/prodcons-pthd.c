70533f01f673d490056b39073f40734785b85376
lukeWaninger <luke.waninger@gmail.com>
2017-01-31 18:17:45 -0800

    .

--- CS415-Parallel-Programming/Homework/1/prodcons-pthd.c
+++ CS415-Parallel-Programming/Homework/1/prodcons-pthd.c
@@ -14,32 +14,33 @@
 #include <unistd.h>
 #include "task-queue.h"
 
-// function declarations
-void consumer(void*);
-void producer(void);
-
 // default values
-int numConsumers = 1;    // default number of consumers
-int queue_cap    = 20;   // maximum number of items in the queue
-int num_tasks    = 100;  // number of tasks to complete
-queue_t *queue;          // the queue
-pthread_mutex_t buffer;  // lock for task load/unload
-pthread_cond_t cond_task_available;
-pthread_cond_t cond_free_available;
+int numConsumers = 1;               // default number of consumers
+int queue_cap    = 20;              // the maximum task buffer size
+int num_tasks    = 100;             // number of tasks to complete
+queue_t *queue;                     // the queue
+pthread_mutex_t buffer;             // lock the queue (task buffer)
+pthread_cond_t cond_task_available; // for signalling a task is available
+pthread_cond_t cond_free_available; // for signalling a position is open
 
+// a counting variable with a lock to maintain synchronization
 struct count {
   pthread_mutex_t mutex;
   int num;
 };
-struct count free_count;
-struct count task_count;
+struct count free_count;           // keep track of the free count
+struct count task_count;           // keep track of the task count
 
-static void p_add_task(task_t*);
-static void c_init(struct count*, int);
-static void c_up(struct count*);
-static void c_down(struct count*);
+// function declarations
+void consumer(void*);
+void producer(void);
+static void p_add_task(task_t*);         // just for reuse
+static void c_init(struct count*, int);  // to initialize the counting struct
+static void c_up(struct count*);         // to increase a count
+static void c_down(struct count*);       // to decrease a count
 
 int main(int argc, char **argv) {
+  // check for input and change default consumer count if necessary
   if (argc > 1) {
     if ((numConsumers = atoi(argv[1])) < 1) {
       printf("<numCons> must be greater than 0\n");      
@@ -52,7 +53,7 @@ int main(int argc, char **argv) {
   // initialize the locks
   pthread_mutex_init(&buffer, NULL);
  
-  // initialize the task count
+  // initialize the free and task counts
   c_init(&free_count, queue_cap);
   c_init(&task_count, 0);
 
@@ -63,7 +64,6 @@ int main(int argc, char **argv) {
     pthread_create(&threads[k], NULL, (void*)consumer, (void*)k);
 
   // execute the producer code leaving time for the consumers to start
-  // sleep(1);
   producer();
 
   // wait for consumer threads to terminate
@@ -73,64 +73,71 @@ int main(int argc, char **argv) {
   exit(0);
 }
 
-void producer(void) {
+void 
+producer(void) {
   printf("Producer starting on core %d\n", sched_getcpu());
-
   int t_num = 1;
+  
+  // keep adding tasks until the limit is reached
   while(t_num <= num_tasks) {
-    task_t *t = create_task(t_num, t_num);   // create task
-    p_add_task(t);
-    ++t_num;
+    task_t *t = create_task(t_num, t_num); 
+    p_add_task(t);                             
+    ++t_num;                               
   }
   
   // lock, send termination tasks, unlock
   for (int i = 0; i < numConsumers; i++) {
-    task_t *terminate = create_task(-1, -1); // create task
+    task_t *terminate = create_task(-1, -1);
     p_add_task(terminate);
   }
 }
 
-static void p_add_task(task_t *t) {
-    // check for free space in the buffer
+static void
+p_add_task(task_t *t) {
+    // wait for a free space in the buffer
     pthread_mutex_lock(&free_count.mutex);
     while(free_count.num == 0)
       pthread_cond_wait(&cond_free_available, &free_count.mutex);
     pthread_mutex_unlock(&free_count.mutex);
 
+    // lock the buffer, add the task, decrease the free count
     pthread_mutex_lock(&buffer);
     add_task(queue, t); 
     c_down(&free_count);
     pthread_mutex_unlock(&buffer);
     
-    // increase task count 
+    // increase task count and signal its available
     c_up(&task_count);
     pthread_cond_signal(&cond_task_available);
 }
 
-void consumer(void *p) {
+void 
+consumer(void *p) {
   long k = (long)&p;
   printf("Consumer[%ld] starting on core %d\n", k, sched_getcpu());
 
+  // infinite loop to keep consuming tasks
   int t_consumed = 0;
   while (1) {
-    // check for available task
+    // wait for an available task
     pthread_mutex_lock(&task_count.mutex);
     while (task_count.num == 0) {
       pthread_cond_wait(&cond_task_available, &task_count.mutex);
     }
     pthread_mutex_unlock(&task_count.mutex);
 
-    pthread_mutex_lock(&buffer);              // lock the queue
-    task_t *task = remove_task(queue);        // take a task
+    // lock the buffer, take a task, decrease the task count
+    pthread_mutex_lock(&buffer);              
+    task_t *task = remove_task(queue);        
     c_down(&task_count);
-    pthread_mutex_unlock(&buffer);            // unlock
+    pthread_mutex_unlock(&buffer);            
 
-    // increase the free count
+    // increase the free count and signal a free position is available
     c_up(&free_count);
     pthread_cond_signal(&cond_free_available);
 
-    // check for termination task
-    if (task->low == -1 && task->high == -1) {
+    // check for termination task, print number of tasks, terminate
+    if (!task || (task->low == -1 && task->high == -1)) {
       printf("C[%ld]: %d\n", k, t_consumed);
       return;
     }
@@ -139,18 +146,21 @@ void consumer(void *p) {
   }
 }
 
-static void c_init(struct count* c, int init_num) {
+static void 
+c_init(struct count* c, int init_num) {
   pthread_mutex_init(&c->mutex, NULL);
   c->num = init_num;
 }
 
-static void c_down(struct count *c) {
+static void 
+c_down(struct count *c) {
   pthread_mutex_lock(&c->mutex);
   c->num--;
   pthread_mutex_unlock(&c->mutex);
 }
 
-static void c_up(struct count *c) {
+static void 
+c_up(struct count *c) {
   pthread_mutex_lock(&c->mutex);
   c->num++;
   pthread_mutex_unlock(&c->mutex);
